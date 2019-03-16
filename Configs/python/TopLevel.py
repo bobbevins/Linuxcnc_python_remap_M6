6a4c96ec6649f2d8d903fd38efa8bfd06381d07a
bobbevins <48592832+bobbevins@users.noreply.github.com>
2019-03-16 08:55:07 -0400

    Add files via upload

--- Configs/python/TopLevel.py
+++ Configs/python/TopLevel.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
+# Copyright (c) 2017 Michel Trahan
 #from linuxcnc import INTERP_EXECUTE_FINISH, INTERP_OK
-from stdglue import init_stdglue 
 import remap
 
 def __init__(self,**words):
     print len(words)," words passed"
     for w in words:
         print "%s: %s" % (w, words[w])
-    init_stdglue(self)
+
     
 #def pi(self):
 #	return 3.1415926535897932
